56fc30ab905162e60568db110caeb53dcfa9a879
Vi <niniebox-all@yahoo.fr>
2019-01-19 19:29:27 +0100

    Init PostureDetector app

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,151 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+// (c) 2014 Don Coleman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+/* global mainPage, deviceList, refreshButton */
+/* global detailPage, resultDiv, messageInput, sendButton, disconnectButton */
+/* global ble, cordova  */
+/* jshint browser: true , devel: true*/
+'use strict';
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+// ASCII only
+function bytesToString(buffer) {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+// ASCII only
+function stringToBytes(string) {
+    var array = new Uint8Array(string.length);
+    for (var i = 0, l = string.length; i < l; i++) {
+        array[i] = string.charCodeAt(i);
     }
+    return array.buffer;
+}
+
+// this is RedBear Lab's UART service
+var redbear = {
+serviceUUID: "713D0000-503E-4C75-BA94-3148F18D941E",
+txCharacteristic: "713D0003-503E-4C75-BA94-3148F18D941E", // transmit is from the phone's perspective
+rxCharacteristic: "713D0002-503E-4C75-BA94-3148F18D941E"  // receive is from the phone's perspective
 };
 
-app.initialize();
+var app = {
+initialize: function() {
+    this.bindEvents();
+    detailPage.hidden = true;
+
+    localStorage.setItem("ok",1);
+},
+bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+    refreshButton.addEventListener('touchstart', this.refreshDeviceList, false);
+    sendButton.addEventListener('click', this.sendData, false);
+    disconnectButton.addEventListener('touchstart', this.disconnect, false);
+    deviceList.addEventListener('touchstart', this.connect, false); // assume not scrolling
+},
+onDeviceReady: function() {
+    app.refreshDeviceList();
+},
+refreshDeviceList: function() {
+    deviceList.innerHTML = ''; // empties the list
+    if (cordova.platformId === 'android') { // Android filtering is broken
+        ble.scan([], 5, app.onDiscoverDevice, app.onError);
+    } else {
+        ble.scan([redbear.serviceUUID], 5, app.onDiscoverDevice, app.onError);
+    }
+},
+onDiscoverDevice: function(device) {
+    var listItem = document.createElement('li'),
+    html = '<b>' + device.name + '</b><br/>' +
+    'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
+    device.id;
+
+    listItem.dataset.deviceId = device.id;
+    listItem.innerHTML = html;
+    deviceList.appendChild(listItem);
+},
+connect: function(e) {
+    var deviceId = e.target.dataset.deviceId,
+    onConnect = function() {
+        // subscribe for incoming data
+        ble.startNotification(deviceId, redbear.serviceUUID, redbear.rxCharacteristic, app.onData, app.onError);
+        sendButton.dataset.deviceId = deviceId;
+        disconnectButton.dataset.deviceId = deviceId;
+        app.showDetailPage();
+    };
+
+    ble.connect(deviceId, onConnect, app.onError);
+},
+onData: function(data) { // data received from Arduino
+    //console.log(data);
+    resultDiv.innerHTML = resultDiv.innerHTML + "Received: " + bytesToString(data) + "<br/>";
+    resultDiv.scrollTop = resultDiv.scrollHeight;
+
+    console.log(bytesToString(data));
+
+    if(bytesToString(data) != "warning" && bytesToString(data) != "wrong")
+    {
+        var value = parseFloat(bytesToString(data));
+
+        if(localStorage.getItem("ok") == 1){
+            localStorage.setItem("ok", value);
+        }
+
+        if(bytesToString(data).substring(0, 4) == "back" ){
+            localStorage.setItem("ok", 1);
+        }
+
+
+        var monCanvas = document.getElementById("canvas1");
+        var ctx = monCanvas.getContext("2d");
+        ctx.clearRect(0,0,monCanvas.width, monCanvas.height);
+
+        ctx.beginPath(); // commencer à dessiner
+        ctx.moveTo(localStorage.getItem("ok"),localStorage.getItem("ok"));
+        ctx.lineTo(150,value);
+        ctx.stroke();
+    }
+},
+sendData: function(event) { // send data to Arduino
+
+    var success = function() {
+        console.log("success");
+        resultDiv.innerHTML = resultDiv.innerHTML + "Sent: " + messageInput.value + "<br/>";
+        resultDiv.scrollTop = resultDiv.scrollHeight;
+    };
+
+    var failure = function() {
+        alert("Failed writing data to the redbear hardware");
+    };
+
+    var data = stringToBytes(messageInput.value);
+    var deviceId = event.target.dataset.deviceId;
+    ble.writeWithoutResponse(deviceId, redbear.serviceUUID, redbear.txCharacteristic, data, success, failure);
+
+},
+disconnect: function(event) {
+    var deviceId = event.target.dataset.deviceId;
+    ble.disconnect(deviceId, app.showMainPage, app.onError);
+},
+showMainPage: function() {
+    mainPage.hidden = false;
+    detailPage.hidden = true;
+},
+showDetailPage: function() {
+    mainPage.hidden = true;
+    detailPage.hidden = false;
+},
+onError: function(reason) {
+    alert("ERROR: " + reason); // real apps should use notification.alert
+}
+};
